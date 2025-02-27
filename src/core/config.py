010a9c5955a98dadc3ddf012b7affe1ed53ee6f7
João Godoi <joaogodoi.dev@gmail.com>
2025-02-26 23:42:15 -0300

    Created core

---
+++ src/core/config.py
@@ -0,0 +1,12 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Settings:
+    DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+
+
+settings = Settings()
